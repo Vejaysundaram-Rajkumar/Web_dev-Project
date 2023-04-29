66a83d5178ccd4f04218cadc1ec703cd9f941814
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2023-04-29 13:22:18 +0530

    update 2

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
 @app.route('/')
@@ -15,5 +14,16 @@ def signin():
     return render_template("sign-up.html")
 
 
+@app.route('/login')
+def login():
+    return render_template("log-in.html")
+@app.route('/pp')
+def pp():
+    return render_template("privacy-policy.html")
+@app.route('/tac')
+def tac():
+    return render_template("terms-conditions.html")
+
+
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0")
+    app.run(host="0.0.0.0")
